7af9c904425f6ecb03288c117b18aace06708e29
trung hậu đinh <trunghaukmhd@gmail.com>
2020-10-27 23:38:50 +0700

    Add item food

--- lib/ui/food_page/foods_page.dart
+++ lib/ui/food_page/foods_page.dart
@@ -14,16 +14,83 @@ class FoodPage extends StatelessWidget {
   Widget build(BuildContext context) {
     Map<String, Category> arguments = ModalRoute.of(context).settings.arguments;
     this.category = arguments['category'];
-    List<Food> listFood = FAKE_FOODS.where((food) => food.categoryId == category.id).toList();
+    List<Food> listFood =
+        FAKE_FOODS.where((food) => food.categoryId == category.id).toList();
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('Menu\'s ${category.content}'),
       ),
       body: Center(
-        // child: ListView.builder(
-        //   itemBuilder
-        // ),
+        child: ListView.builder(
+            itemCount: listFood.length,
+            itemBuilder: (context, index) {
+              Food food = listFood[index];
+              return InkWell(
+                onTap: () {},
+                child: Stack(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        clipBehavior: Clip.hardEdge,
+                        child: Center(
+                          child: FadeInImage.assetNetwork(
+                              placeholder: 'assets/images/loading.gif',
+                              image: food.urlName),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 30,
+                      left: 30,
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: Colors.lightBlue,
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(color: Colors.white, width: 2)),
+                        child: Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.timer,
+                              color: Colors.white,
+                              size: 26,
+                            ),
+                            Text(
+                              "${food.duration.inMinutes} minutes",
+                              style: TextStyle(color: Colors.white),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 30,
+                      top: 30,
+                      child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.greenAccent,
+                              borderRadius: BorderRadius.circular(8)),
+                          padding: const EdgeInsets.all(5),
+                          child: Text(
+                              "${food.complexity.toString().split('.').last}")),
+                    ),
+                    Positioned(
+                      right: 30,
+                      bottom: 30,
+                      child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.black26,
+                              borderRadius: BorderRadius.circular(8)),
+                          padding: const EdgeInsets.all(5),
+                          child: Text("${food.name}",style: TextStyle(color: Colors.white),)),
+                    )
+                  ],
+                ),
+              );
+            }),
       ),
     );
   }
